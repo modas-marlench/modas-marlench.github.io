1412bd65629190a66be6bf8605d4590230688cda
modas-marlench <108901394+modas-marlench@users.noreply.github.com>
2023-02-10 12:11:16 -0500

    Update ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
     // AJAX para traer oficinas
     $.ajax({
         type: "GET",
-        url: "https://sheets.googleapis.com/v4/spreadsheets/1mS3E0THHG2J85CeN6tm0WsPT5ZEkjsvykcw9byhCljw/values/OFICINAS2!B2:D?key=AIzaSyDksVE4q2AIAA4-IGrPgM007Wnd_K17bGk",
+        url: "https://sheets.googleapis.com/v4/spreadsheets/1mS3E0THHG2J85CeN6tm0WsPT5ZEkjsvykcw9byhCljw/values/OFICINAS!B2:D?key=AIzaSyDksVE4q2AIAA4-IGrPgM007Wnd_K17bGk",
         dataType: 'json',
         success: function(data) {
             data.values.forEach(function(dato) {
